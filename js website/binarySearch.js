d2245fb96d77af9a449ad893d1cba26d9beb45c9
Vishnuraj Saravanan <76027763+vishnu02-droid@users.noreply.github.com>
2021-12-28 18:08:00 +0530

    Add files via upload

---
+++ js website/binarySearch.js
@@ -0,0 +1,26 @@
+//divide and conquer
+function binarySearch(array,key){
+    let start = 0;
+    let end = array.length - 1;
+    while(start <= end){
+        let middle = Math.floor((start + end)/2);
+        if(array[middle] === key) {
+            return middle;
+        }
+        else if(array[middle] < key){
+            start = middle + 1;
+        }
+        else {
+            end = middle - 1;
+        }
+    }
+    return -1;
+}
+
+console.log(binarySearch([1,8,4,7,2,6,9],9));
+//time O(n)
+// in place algorithm - which means it works directly on the original array without making any copies
+// it works only on sorted arrays - O(nlogn)
+// if the array is smaller, we need to search it only once, a brute-force (linear search) algo might be better
+
+console.time(binarySearch);
